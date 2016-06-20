ef79d8de5040f7ce3e772da000e2a5cfffbcf409
Saad Quadri <saad@saadquadri.com>
2016-06-20 13:57:46 -0700

    Add plugin

---
+++ MozillaQuery.py
@@ -0,0 +1,71 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import sublime
+import sublime_plugin
+import webbrowser
+
+class MdnSearchCommand(sublime_plugin.ApplicationCommand):
+    """ Command for searching through MDN """
+    def run(self):
+        text = "MDN Search Query:"
+        sublime.active_window().show_input_panel(text, "", self.on_query, self.on_change, self.on_cancel)
+
+    def on_query(self, query):
+        url = "https://developer.mozilla.org/en-US/search?q={0}".format(query)
+        webbrowser.open_new_tab(url)
+
+    def on_change(self, input):
+        pass
+
+    def on_cancel(self):
+        pass
+
+class DxrSearchCommand(sublime_plugin.ApplicationCommand):
+    """ Command for doing a code search through DXR """
+    def run(self):
+        text = "DXR Search Query:"
+        sublime.active_window().show_input_panel(text, "", self.on_query, self.on_change, self.on_cancel)
+
+    def on_query(self, query):
+        url = 'https://dxr.mozilla.org/mozilla-central/search?q={0}&redirect=false'.format(query)
+        webbrowser.open_new_tab(url)
+
+    def on_change(self, input):
+        pass
+
+    def on_cancel(self):
+        pass
+
+class DxrfSearchCommand(sublime_plugin.ApplicationCommand):
+    """ Command for doing a file search through DXR """
+    def run(self):
+        text = "DXR File Search Query:"
+        sublime.active_window().show_input_panel(text, "", self.on_query, self.on_change, self.on_cancel)
+
+    def on_query(self, query):
+        url = 'https://dxr.mozilla.org/mozilla-central/search?q=file:{0}&redirect=false'.format(query)
+        webbrowser.open_new_tab(url)
+
+    def on_change(self, input):
+        pass
+
+    def on_cancel(self):
+        pass
+
+class BugzillaSearchCommand(sublime_plugin.ApplicationCommand):
+    """ Command for searching through Bugzilla """
+    def run(self):
+        text = "Bugzilla Search Query: "
+        sublime.active_window().show_input_panel(text, "", self.on_query, self.on_change, self.on_cancel)
+
+    def on_query(self, query):
+        url = 'https://bugzilla.mozilla.org/buglist.cgi?quicksearch={0}'.format(query)
+        webbrowser.open_new_tab(url)
+
+    def on_change(self, input):
+        pass
+
+    def on_cancel(self):
+        pass
